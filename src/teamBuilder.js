c63435d3e1da8b136255fdc11f88002dcc2a9a8a
Craig <panamaworldwideinvestments@gmail.com>
2022-07-26 14:05:15 +0100

    changed file destination for lib in team builder.js

--- src/teamBuilder.js
+++ src/teamBuilder.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-const Manager = require("./lib/Manager");
-const Intern = require("./lib/Intern");
-const Engineer = require("./lib/Engineer");
+const Manager = require('..//lib/Manager');
+const Intern = require('..//lib/Intern');
+const Engineer = require('..//lib/Engineer');
 
 const buildTeamPage = (managers,interns, engineers ) => {
     let html= fs.readFileSync('./template/index.html, utf8');
@@ -55,7 +55,6 @@ const buildTeamPage = (managers,interns, engineers ) => {
     }
 }
 
-module.exports = teamBuilder;
 
 
         
